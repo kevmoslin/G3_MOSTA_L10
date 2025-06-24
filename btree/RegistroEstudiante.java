d957bedc5f277de3aef0309bc9229f2bc1deccb6
kevmoslin <kevinmostacerolinares@gmail.com>
2025-06-24 12:10:45 -0500

    atributos y getters

--- btree/RegistroEstudiante.java
+++ btree/RegistroEstudiante.java
@@ -1,4 +1,21 @@
 package btree;
-public class RegistroEstudiante {
+
+public class RegistroEstudiante implements Comparable<RegistroEstudiante>{
+    private int codigo;
+    private String nombre;
+
+    public RegistroEstudiante(int codigo, String nombre){
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public int getCodigo(){
+        return codigo;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
     
 }
